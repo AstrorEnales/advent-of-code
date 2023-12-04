06ad32bd14377261a40a23902f3582606212c2a3
Marcel Friedrichs <astror.enales@gmail.com>
2023-12-04 09:52:31 +0100

    Remove debug prints

--- 2023-12-03/test_01.py
+++ 2023-12-03/test_01.py
@@ -39,21 +39,16 @@ with open('input_01.txt', 'r', encoding='utf-8') as f:
             part_number = int(line[start:end + 1])
             is_adjacent_to_symbol = False
             if start > 0 and is_allowed_symbol(line[start - 1]):
-                print('1', part_number)
                 is_adjacent_to_symbol = True
             if end < len(line) - 1 and is_allowed_symbol(line[end + 1]):
-                print('2', part_number)
                 is_adjacent_to_symbol = True
             for j in range(max(0, start - 1), min(end + 2, len(line))):
                 if i > 0 and is_allowed_symbol(lines[i - 1][j]):
-                    print('3', part_number)
                     is_adjacent_to_symbol = True
                 if i < len(lines) - 1 and is_allowed_symbol(lines[i + 1][j]):
-                    print('4', part_number)
                     is_adjacent_to_symbol = True
 
             if is_adjacent_to_symbol:
                 part_numbers.append(part_number)
-        print(part_numbers)
         result += sum(part_numbers)
     print(result)
